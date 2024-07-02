7a2e25c96d590ab28e5ac6c87ab2f704d0516dee
Mynor Baran <tubismmm@gmail.com>
2024-07-01 22:27:03 -0600

    Initial commit

---
+++ src/main/java/com/alurachallenge/challenge_literatura/ChallengeLiteraturaApplication.java
@@ -0,0 +1,13 @@
+package com.alurachallenge.challenge_literatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeLiteraturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeLiteraturaApplication.class, args);
+	}
+
+}
